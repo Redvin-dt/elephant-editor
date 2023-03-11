ba80c22bd4b2ea015a0c71023cec5602ba63044d
Redvin <pavel.afonkin.04@mail.ru>
2023-03-11 18:04:56 +0300

    remove extra files

--- Latex-editor/build/CMakeFiles/Latex-editor.dir/Latex-editor_autogen/3YJK5W5UP7/qrc_codeeditor_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Latex-editor.dir/Latex-editor_autogen/3YJK5W5UP7/qrc_codeeditor_resources.cpp.o: \
- /home/pavel/HSE\ project/main_project_folder/elephant-editor/Latex-editor/build/Latex-editor_autogen/3YJK5W5UP7/qrc_codeeditor_resources.cpp \
- /usr/include/stdc-predef.h
