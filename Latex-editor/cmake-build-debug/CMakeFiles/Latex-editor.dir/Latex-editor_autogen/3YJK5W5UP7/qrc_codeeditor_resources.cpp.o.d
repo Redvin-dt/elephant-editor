1bd80b20c3cba3b07aee727f94b87c51c141781f
romanpovol <97195569+romanpovol@users.noreply.github.com>
2023-06-24 21:26:55 +0300

    Delete Latex-editor/cmake-build-debug directory

--- Latex-editor/cmake-build-debug/CMakeFiles/Latex-editor.dir/Latex-editor_autogen/3YJK5W5UP7/qrc_codeeditor_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Latex-editor.dir/Latex-editor_autogen/3YJK5W5UP7/qrc_codeeditor_resources.cpp.o: \
- /home/roman/project_server/elephant-editor/Latex-editor/cmake-build-debug/Latex-editor_autogen/3YJK5W5UP7/qrc_codeeditor_resources.cpp \
- /usr/include/stdc-predef.h
